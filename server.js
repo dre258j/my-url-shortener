efaa9b8e421a6ee71276d094a522a79fae411918
dre258j <dre258j@gmail.com>
2025-04-11 03:14:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname));
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
