9978d9517410b5b24456b0da5b0bb569d65cb495
dre258j <dre258j@gmail.com>
2025-04-11 01:57:52 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+let urlDatabase = {};
+let counter = 1;
+
+// Shorten URL logic
+app.post('/shorten', (req, res) => {
+    const { url } = req.body;
+    const shortenedUrl = `https://${req.headers.host}/${counter}`;
+    urlDatabase[counter] = url;
+    counter++;
+    res.json({ shortened_url: shortenedUrl });
+});
+
+// Redirect to original URL
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const originalUrl = urlDatabase[id];
+    if (originalUrl) {
+        res.redirect(originalUrl);
+    } else {
+        res.status(404).send('URL not found');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
